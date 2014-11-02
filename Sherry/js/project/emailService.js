20d3ce51926ae842a8de88f206625d057a48f47c
hanjia2014 <hanj@datacom.co.nz>
2014-11-03 08:37:59 +1300

    Create

---
+++ Sherry/js/project/emailService.js
@@ -0,0 +1,31 @@
+﻿myApp.factory(
+            "emailService",
+            function ($http, $q) {
+
+                // Return public API.
+                return ({
+                    send: function (emailInstance) {
+                        return $http({
+                            method: "post",
+                            url: "https://mandrillapp.com/api/1.0/messages/send.json",
+                            data: {
+                                'key': '71V5dukB9IRb7F7gEV0Qzg',
+                                'message': {
+                                    'from_email': emailInstance.from,
+                                    'to': [
+                                        {
+                                            'email': 'hummvee@gmail.com',
+                                            'name': emailInstance.name,
+                                            'type': 'to'
+                                        }
+                                    ],
+                                    'autotext': 'true',
+                                    'subject': emailInstance.name + ' - ' + emailInstance.phone,
+                                    'html': '<p>Email recieved from ' + emailInstance.name + '</p>' + emailInstance.message
+                                }
+                            }
+                        });
+                    },
+
+                });
+            });
---
+++ Sherry/obj/Release/Package/PackageTmp/js/project/homeCtrl.js
@@ -0,0 +1,24 @@
+﻿myApp.controller('homeCtrl', function ($scope, emailService) {
+    $scope.copyRight = "Han Jia " + new Date().getFullYear();
+    $scope.sendEmail = function () {
+
+        if (checkNullOrEmpty($scope.youName)
+            || checkNullOrEmpty($scope.youPhone)
+            || checkNullOrEmpty($scope.emailMessage)
+            || checkNullOrEmpty($scope.emailAddress)) return;
+
+        var emailInstance = {};
+        emailInstance.name = $scope.youName;
+        emailInstance.phone = $scope.youPhone;
+        emailInstance.message = $scope.emailMessage;
+        emailInstance.from = $scope.emailAddress;
+
+        emailService.send(emailInstance).success(function () {
+            $scope.isEmailSent = true;
+        }).error(function () { });
+    };
+
+    function checkNullOrEmpty(value) {
+        return value == null || value == '';
+    };
+});
